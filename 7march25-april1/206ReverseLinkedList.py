258629204888566864d49a4e7e9bdb8a59193121
Vladyslav Kinash <112383503+VladKinash@users.noreply.github.com>
2024-03-27 16:21:12 +0200

    Add files via upload

---
+++ 7march25-april1/206ReverseLinkedList.py
@@ -0,0 +1,12 @@
+def reverseList(self, head):
+
+    curr = head
+    prev = None
+
+    while curr:
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+
+    return prev
